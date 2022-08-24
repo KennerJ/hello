45a4951f73e36512474ff986a22c09cae022a9b2
Kenner Jensen <kjensen25@huskers.unl.edu>
2022-08-24 20:04:15 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author Kenner Jensen
+ *        kjensen25@huskers.unl.edu
+ * Date: 2022/21/08
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Kenner Jensen, Computer Science\n");
+    printf("Ben Seitz, Electrical Engineering\n");
+    return 0;
+ }
